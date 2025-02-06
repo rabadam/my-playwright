19e45b0c65e057135fc3a39f33ec0e416ce98cab
rabadam <ruel.abadam@headspace.com>
2025-02-06 15:10:00 -0800

    Update example.spec.js

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,1 +1,6 @@
-#code
+import { test, expect } from '@playwright/test';
+
+test('Check if home page loads', async ({ page }) => {
+  await page.goto('https://ruelabadam.com');
+  await expect(page).toHaveTitle(/Ruel Abadam/i);
+});
